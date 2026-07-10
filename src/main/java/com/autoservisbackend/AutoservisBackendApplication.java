d6b655280ae470e61b4547d56668e15ccc625ef2
Lazar Lukic <krazihero05@gmail.com>
2026-07-10 17:13:03 +0200

    inicijalna konfiguracija spring boot projekta

---
+++ src/main/java/com/autoservisbackend/AutoservisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autoservisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoservisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoservisBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autoservisbackend/AutoservisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoservisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoservisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
