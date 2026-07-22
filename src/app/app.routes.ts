e7aa9950d1387bf08a500de26096e280770eafc3
Lazar Lukic <krazihero05@gmail.com>
2026-07-22 15:48:58 +0200

    feat: dodani servisi, interceptor, guard i rute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'user',
+    loadComponent: () => import('./user/user.component').then(m => m.UserComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
